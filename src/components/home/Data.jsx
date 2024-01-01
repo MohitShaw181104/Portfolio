83a1ec41fab6bb81341b116677c3e5029ffa1104
Mohit <mohitshaw181104@gmail.com>
2024-01-01 20:19:24 +0530

    added social icon

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">Mohit Shaw
+        <img src='assets/1.png'/>
+        </h1>
+    </div>
+  )
+}
+
+export default Data
