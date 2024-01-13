dfc4aa2a04ae8b39e2774fafa762743de2bf8d37
Mohit <mohitshaw181104@gmail.com>
2024-01-13 23:18:23 +0530

    added project section

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,56 @@
+import Work1 from '../../assets/Work1.png'
+
+export const projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "AI Chatbot",
+      category: "Web",
+    },
+    {
+        id: 2,
+        image: Work1,
+        title: "AI Fatbot",
+        category: "Web",
+    },
+    {
+        id: 3,
+        image: Work1,
+        title: "AI Fatbot",
+        category: "Web",
+    },
+    {
+        id: 4,
+        image: Work1,
+        title: "AI Fatbot",
+        category: "Web",
+    },
+    {
+        id: 5,
+        image: Work1,
+        title: "AI Fatbot",
+        category: "App",
+    },
+    {
+        id: 6,
+        image: Work1,
+        title: "AI Fatbot",
+        category: "Design",
+    },
+  ];
+
+export const projectsNav = [
+    {
+        name: 'All',
+    },
+    {
+        name: 'Web',
+    },
+    {
+        name: 'App',
+    },
+    {
+        name: 'Design',
+    },
+    
+]
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './work.css'
+import Works from './Works'
 
 const Work = () => {
   return (
-    <section>
-        Work
+    <section className="work section" id='portfolio'>
+      <h2 className="section__title">Portfolio</h2>
+      <span className="section__subtitle">Most Recent Works</span>
+
+      <Works/>
     </section>
   )
 }
